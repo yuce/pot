381d427bf1be6d37fa24f7a6bef856a1cc43bf4f
Julius Beckmann <julius.beckmann@zenner-iot.com>
2018-02-09 13:53:48 +0100

    Added option `timestamp` to create totp for a given time.

--- test/totp_generation_tests.erl
+++ test/totp_generation_tests.erl
@@ -8,6 +8,10 @@ generating_current_totp_and_validating_test_() ->
             fun stop/1,
             fun generating_current_totp_and_validating/1}.
 
+generating_totp_for_given_timestamp_and_compare_test_() ->
+    {setup, fun start/0,
+        fun stop/1,
+        fun generating_totp_for_given_timestamp_and_compare/1}.
 
 start() ->
     ok.
@@ -24,6 +28,10 @@ generating_current_totp_and_validating(_) ->
     Totp = pot:totp(Secret),
     [?_assertEqual(Hotp, Totp)].
 
+generating_totp_for_given_timestamp_and_compare(_) ->
+    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+    Totp = pot:totp(Secret, [{timestamp, {1518, 179058, 919315}}]),
+    [?_assertEqual(Totp, <<"151469">>)].
 
 time_now() ->
     {MegaSecs, Secs, _} = os:timestamp(),
