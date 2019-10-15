074100f369d34069eed9e55b7edb03bead6b0e89
Nicholas Lundgaard <nlundgaard@alertlogic.com>
2019-10-15 01:45:51 -0500

    Fix valid_totp to support upper bound on check_candidate
    Resolves #18. The logic of `pot: valid_totp/2,3` incorrectly fails to evaluate the highest candidate when the `window` is set to a value greater than `0`. This is because the logic of `pot:check_candidate/5` short-circuits on `Current == Last` rather than `Current > Last`, which is what would be needed for it to work as expected.
    
    This change alters `pot:check_candidate/5` to break on `Current > Last`, and updates the unit tests accordingly. Note that, because `pot:valid_hotp/2,3` **also** use  `pot:check_candidate/5`, this change extends by 1 the number of candidate trials that it will check when evaluating a hotp Token. This appears to also be a bug fix, because if you set `trials` to `1`, previously the next valid hotp Token would be rejected.
    
    Unit tests have been added to cover the cases described above.

--- test/totp_validity_tests.erl
+++ test/totp_validity_tests.erl
@@ -3,22 +3,15 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
-validating_totp_for_same_secret_test_() ->
-    {setup, fun start/0,
-            fun stop/1,
-            fun validating_totp_for_same_secret/1}.
-
-
-validating_invalid_totp_for_same_secret_test_() ->
-    {setup, fun start/0,
-            fun stop/1,
-            fun validating_invalid_totp_for_same_secret/1}.
-
-
-validating_correct_hotp_as_totp_test_() ->
-    {setup, fun start/0,
-            fun stop/1,
-            fun validating_correct_hotp_as_totp/1}.
+totp_validity_test_() ->
+    {foreach,
+     fun start/0,
+     fun stop/1,
+     [fun validating_totp_for_same_secret/1,
+      fun validating_invalid_totp_for_same_secret/1,
+      fun validating_correct_hotp_as_totp/1,
+      fun validating_past_future_totp_too_small_window/1,
+      fun validating_past_future_totp_with_window/1]}.
 
 
 start() ->
@@ -31,14 +24,34 @@ stop(_) ->
 
 validating_totp_for_same_secret(_) ->
     Secret = <<"MFRGGZDFMZTWQ2LK">>,
-    [?_assertEqual(pot:valid_totp(pot:totp(Secret), Secret), true)].
+    [?_assert(pot:valid_totp(pot:totp(Secret), Secret))].
 
 
 validating_invalid_totp_for_same_secret(_) ->
     Secret = <<"MFRGGZDFMZTWQ2LK">>,
-    [?_assertEqual(pot:valid_totp(<<"123456">>, Secret), false)].
+    [?_assertNot(pot:valid_totp(<<"123456">>, Secret))].
 
 
 validating_correct_hotp_as_totp(_) ->
     Secret = <<"MFRGGZDFMZTWQ2LK">>,
-    [?_assertEqual(pot:valid_totp(pot:hotp(Secret, 1), Secret), false)].
+    [?_assertNot(pot:valid_totp(pot:hotp(Secret, 1), Secret))].
+
+
+validating_past_future_totp_too_small_window(_) ->
+    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+    IntervalOpts = [{timestamp, os:timestamp()}],
+    N = 5,
+    [?_assertNot(pot:valid_totp(pot:totp(Secret, [{addwindow, AW} | IntervalOpts]),
+                                Secret,
+                                [{window, W} | IntervalOpts]))
+     || W <- lists:seq(0, N), AW <- lists:seq(-N, N), W < abs(AW)].
+
+
+validating_past_future_totp_with_window(_) ->
+    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+    IntervalOpts = [{timestamp, os:timestamp()}],
+    N = 5,
+    [?_assert(pot:valid_totp(pot:totp(Secret, [{addwindow, AW} | IntervalOpts]),
+                             Secret,
+                             [{window, W} | IntervalOpts]))
+     || W <- lists:seq(0, N), AW <- lists:seq(-N, N), W >= abs(AW)].
