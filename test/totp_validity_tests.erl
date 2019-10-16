d7abe97eacbea665fcb0e6dc8fff1a0adbc12484
Nicholas Lundgaard <nlundgaard@alertlogic.com>
2019-10-16 01:05:22 -0500

    Simplify and extend unit tests
    * Use a single unified eunit test generator for each module's tests, to reduce repetition.
    * Leverage the `setup/0` function for storing common test parameters.
    * Add tests of invalid tokens in `pot:valid_hotp` and `pot:valid_totp` to achieve full line coverage in unit tests.
    * extend hotp_generation_tests to reason about 0 and negative/very large interval (counter) values.

--- test/totp_validity_tests.erl
+++ test/totp_validity_tests.erl
@@ -9,36 +9,37 @@ totp_validity_test_() ->
      fun stop/1,
      [fun validating_totp_for_same_secret/1,
       fun validating_invalid_totp_for_same_secret/1,
+      fun validatinging_invalid_token_hotp/1,
       fun validating_correct_hotp_as_totp/1,
       fun validating_past_future_totp_too_small_window/1,
       fun validating_past_future_totp_with_window/1]}.
 
 
 start() ->
-    ok.
+    _Secret = <<"MFRGGZDFMZTWQ2LK">>.
 
 
 stop(_) ->
     ok.
 
 
-validating_totp_for_same_secret(_) ->
-    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+validating_totp_for_same_secret(Secret) ->
     [?_assert(pot:valid_totp(pot:totp(Secret), Secret))].
 
 
-validating_invalid_totp_for_same_secret(_) ->
-    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+validating_invalid_totp_for_same_secret(Secret) ->
     [?_assertNot(pot:valid_totp(<<"123456">>, Secret))].
 
 
-validating_correct_hotp_as_totp(_) ->
-    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+validatinging_invalid_token_hotp(Secret) ->
+     [?_assertNot(pot:valid_totp(<<"abcdef">>, Secret))].
+
+
+validating_correct_hotp_as_totp(Secret) ->
     [?_assertNot(pot:valid_totp(pot:hotp(Secret, 1), Secret))].
 
 
-validating_past_future_totp_too_small_window(_) ->
-    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+validating_past_future_totp_too_small_window(Secret) ->
     IntervalOpts = [{timestamp, os:timestamp()}],
     N = 5,
     [?_assertNot(pot:valid_totp(pot:totp(Secret, [{addwindow, AW} | IntervalOpts]),
@@ -47,8 +48,7 @@ validating_past_future_totp_too_small_window(_) ->
      || W <- lists:seq(0, N), AW <- lists:seq(-N, N), W < abs(AW)].
 
 
-validating_past_future_totp_with_window(_) ->
-    Secret = <<"MFRGGZDFMZTWQ2LK">>,
+validating_past_future_totp_with_window(Secret) ->
     IntervalOpts = [{timestamp, os:timestamp()}],
     N = 5,
     [?_assert(pot:valid_totp(pot:totp(Secret, [{addwindow, AW} | IntervalOpts]),
